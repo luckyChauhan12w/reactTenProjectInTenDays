a24dd8a9631934aa497c335984b2d7985bae4437
Lucky Chauhan <luckychauhan.12w@gmail.com>
2025-08-14 23:20:06 +0530

    Blog

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -3,12 +3,12 @@ import Blog from "./components/Blog";
 
 const App = () => {
   return (
-    <div className="px-4">
-      <nav className="flex justify-between py-2">
-        <h1 className="text-[#732DEB] font-bold text-2xl">LastBlog</h1>
+    <div className="bg-[#101828]">
+      <nav className="w-[100%] border-b border-[#364153] bg-[#03071289] backdrop-blur-md text-white flex sticky top-0 left-0 justify-between px-4 py-2">
+        <h1 className="text-[#00BCFF] font-semibold text-2xl">LastBlog</h1>
         <div className="w-[35px] h-[35px] bg-gray-400 rounded-full"></div>
       </nav>
-      <div>
+      <div className="px-4 text-white">
         <Blog />
       </div>
     </div>
--- Blog/src/components/Blog.jsx
+++ Blog/src/components/Blog.jsx
@@ -2,8 +2,35 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>This is Blog Section ...</h1>
+    <div className="min-h-screen pt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-15">
+      {[...Array(10)].map(() => {
+        return (
+          <div className="">
+            <div className="rounded-2xl overflow-hidden">
+              <img
+                src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+                alt=""
+              />
+            </div>
+            <div className="">
+              <div className="flex items-center gap-6 mt-6">
+                <p className="text-[#99A1AF] text-xs">Mar 16, 2020</p>
+                <p className="bg-[#182332] text-[#D1D5DC] py-1 px-3 rounded-full text-xs">
+                  Marketing
+                </p>
+              </div>
+              <h1 className="text-xl mt-2 font-semibold">
+                Boost your conversion rate
+              </h1>
+              <p className="mt-4 text-[#99A1AF]">
+                Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+                exercitationem placeat
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
