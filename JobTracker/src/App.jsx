ef397a54cf38b512722300e3d7f2dc542ad5a082
Lucky Chauhan <luckychauhan.12w@gmail.com>
2025-08-20 05:10:35 +0530

    jobtracker

--- JobTracker/src/App.jsx
+++ JobTracker/src/App.jsx
@@ -1,5 +1,3 @@
-// App.js
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { JobProvider } from "./context/JobContext";
 import Dashboard from "./pages/Dashboard";
